e7c76189584d44c245925a3b8a405e4f29d86452
Igor <igorgunov1564@yandex.ru>
2023-03-15 18:17:48 +0300

    first commit

--- app/src/main/java/ru/mirea/GunovIgor/lesson1/MainActivity.java
+++ app/src/main/java/ru/mirea/GunovIgor/lesson1/MainActivity.java
@@ -2,18 +2,34 @@ package ru.mirea.GunovIgor.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    Button button2;
-    ProgressDialog progressDialog;
+    private Button button;
+    private Button button2;
+    private Button button3;
+    private ProgressDialog progressDialog;
+    private TextView textView;
+    private TextView textView2;
+    DatePickerDialog.OnDateSetListener setListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +37,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.but1);
         button2 = findViewById(R.id.but4);
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressDialog = new ProgressBar(MainActivity.this);
+                progressDialog = new ProgressDialog(MainActivity.this);
                 progressDialog.show();
                 progressDialog.setContentView(R.layout.progress_dialog);
+                progressDialog.getWindow().setBackgroundDrawableResource(
+                        android.R.color.transparent
+                );
             }
         });
+
+
+
+        button3 = findViewById(R.id.but5);
+        textView = findViewById(R.id.text2);
+        button3.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            int hours = calendar.get(Calendar.HOUR_OF_DAY);
+            int mins = calendar.get(Calendar.MINUTE);
+            TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, androidx.appcompat.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    Calendar c = Calendar.getInstance();
+                    c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                    c.set(Calendar.MINUTE, minute);
+                    c.setTimeZone(TimeZone.getDefault());
+                    SimpleDateFormat format = new SimpleDateFormat("k:mm a");
+                    String time = format.format(c.getTime());
+                    textView.setText(time);
+                }
+            }, hours, mins, false);
+            timePickerDialog.show();
+        });
+
+
+
+        textView2 = findViewById(R.id.text3);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        , setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = day + "/" + month + "/" + year;
+                textView2.setText(date);
+            }
+        };
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        progressDialog.dismiss();
     }
 
     public void onClickNewActivity(View view) {
